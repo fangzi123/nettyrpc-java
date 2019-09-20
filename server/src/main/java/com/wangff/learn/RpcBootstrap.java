9797dfc05441815f4161e9f1704a7406a772b428
wangff <1024904798@qq.com>
2019-09-20 11:50:32 +0800

    server springboot化

--- server/src/main/java/com/wangff/learn/RpcBootstrap.java
+++ server/src/main/java/com/wangff/learn/RpcBootstrap.java
@@ -1,11 +1,14 @@
 package com.wangff.learn;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class RpcBootstrap {
 
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext("server-spring.xml");
-//         new AnnotationConfigApplicationContext(AppConfig.class);
+//        new ClassPathXmlApplicationContext("server-spring.xml");
+//        new AnnotationConfigApplicationContext(AppConfig.class);
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(RpcBootstrap.class, args);
     }
 }
