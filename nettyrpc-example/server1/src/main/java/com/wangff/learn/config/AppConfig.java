9bfb6d906d92a8ee29f44684692a06306ac5b19c
wangff <1024904798@qq.com>
2019-09-23 15:11:38 +0800

    框架标准化

---
+++ nettyrpc-example/server1/src/main/java/com/wangff/learn/config/AppConfig.java
@@ -0,0 +1,58 @@
+package com.wangff.learn.config;//package com.wangff.learn.config;
+//
+//import com.nettyrpc.interceptor.ServerCurrentLimitInterceptor;
+//import com.nettyrpc.server.RpcServer;
+//import com.nettyrpc.server.RpcServerOptions;
+//import com.nettyrpc.server.currentlimit.CurrentLimiter;
+//import com.nettyrpc.server.currentlimit.TokenBucketCurrentLimiter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//
+//import java.util.List;
+//
+///**
+// * @author wangff
+// * @date 2019/9/20 11:11
+// */
+//@Configuration
+//@PropertySource("classpath:rpc.properties")
+//public class AppConfig {
+//
+//    @Value("${registry.address}")
+//    private String namingServiceUrl;
+//
+//    @Value("${server.address.host}")
+//    private String host;
+//    @Value("${server.address.port}")
+//    private int port;
+//
+//    @Value("${server.limit.token.bucketSize}")
+//    private int bucketSize;
+//    @Value("${server.limit.token.tokenInputRate}")
+//    private int tokenInputRate;
+//
+//    @Bean
+//    public TokenBucketCurrentLimiter currentLimiter(){
+//        return new TokenBucketCurrentLimiter(bucketSize,tokenInputRate);
+//    }
+//
+//    @Bean
+//    public ServerCurrentLimitInterceptor serverCurrentLimitInterceptor(CurrentLimiter currentLimiter){
+//        return new ServerCurrentLimitInterceptor(currentLimiter);
+//    }
+//
+//    @Bean
+//    public RpcServerOptions rpcServerOptions(ServerCurrentLimitInterceptor serverCurrentLimitInterceptor) {
+//        RpcServerOptions rpcServerOptions=new RpcServerOptions();
+//        rpcServerOptions.setNamingServiceUrl(namingServiceUrl);
+//        rpcServerOptions.setInterceptors(List.of(serverCurrentLimitInterceptor));
+//        return rpcServerOptions;
+//    }
+//
+//    @Bean
+//    public RpcServer rpcServer(RpcServerOptions rpcServerOptions) {
+//        return new RpcServer(host,port,rpcServerOptions);
+//    }
+//}
